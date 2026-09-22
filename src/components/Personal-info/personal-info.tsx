8c3d574d35d833653cd7b6772883ef7030de08dd
Giovane Richard <giovane@rlvtecnologia.com.br>
2026-09-22 15:36:34 -0300

    feat: implements init persona-info step

--- src/components/Personal-info/personal-info.tsx
+++ src/components/Personal-info/personal-info.tsx
@@ -1,7 +1,24 @@
-export const PersonalInfo = () => {
+import { TFormData } from "@/types/form-data";
+import { ChangeEvent } from "react";
+
+type Props = {
+  formData: TFormData;
+  handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
+};
+
+export const PersonalInfo = ({ formData, handleChange }: Props) => {
   return (
     <div>
       <h1>Personal-info</h1>
+      <p>Please provide your name, address, and phone number.</p>
+
+      <section>
+        <input name="name" value={formData.name} onChange={handleChange} />
+
+        <input name="email" value={formData.name} onChange={handleChange} />
+
+        <input name="phone" value={formData.name} onChange={handleChange} />
+      </section>
     </div>
   );
 };
---
+++ src/types/form-data.ts
@@ -0,0 +1,8 @@
+export type TFormData = {
+  name: string;
+  email: string;
+  phone: string;
+  plan: string;
+  billing: string;
+  addons: any[];
+};
